fdecdec983f73046d6e53831c131ed9dc78580c6
Klementiy <klemens.vv@gmail.com>
2020-12-07 21:58:42 +0300

    Ver.1

---
+++ Bank.Vladykin/TableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  TableViewCell.swift
+//  Bank.Vladykin
+//
+//  Created by user on 23/11/2020.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var Currency: UILabel!
+    
+    @IBOutlet weak var Buy: UILabel!
+    
+    @IBOutlet weak var Sell: UILabel!
+    
+}
--- Bank.Vladykin/ViewController.swift
+++ Bank.Vladykin/ViewController.swift
@@ -8,7 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBAction func loginclick(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Авторизация", message: "Введите логин и пароль", preferredStyle: .alert)
+        present(alert, animated: true, completion: nil)
+        let alertLogin = UIAlertAction(title: "Войти", style: .default, handler: { UIAlertAction in
+            self.performSegue(withIdentifier: "mySegue", sender: nil)
+        })
+        let alertCancel = UIAlertAction(title: "Отмена", style: .default, handler: nil)
+        alert.addAction(alertLogin)
+        alert.addAction(alertCancel)
+        alert.addTextField { (loginTF) in
+            loginTF.placeholder = "Login"
+    }
+        alert.addTextField { (passwordTF) in
+            passwordTF.placeholder = "Password"
+            passwordTF.isSecureTextEntry = true
+        }
+    
+    }
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
